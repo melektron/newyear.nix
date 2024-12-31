928634f925ef26da9926d671688737fd2e083777
melektron <36113305+melektron@users.noreply.github.com>
2024-12-31 23:31:27 +0100

    created flake

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Greetings and happy 2025!";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        my-name = "newyear";
+        my-buildInputs = with pkgs; [ figlet lolcat ];
+        my-script = pkgs.writeShellScriptBin my-name ''
+        figlet Happy new year 2025! At least Something more than goarnix. | lolcat
+        '';
+      in rec {
+        defaultPackage = packages.newyear;
+        packages.newyear = pkgs.symlinkJoin {
+          name = my-name;
+          paths = [ my-script ] ++ my-buildInputs;
+          buildInputs = [ pkgs.makeWrapper ];
+          postBuild = "wrapProgram $out/bin/${my-name} --prefix PATH : $out/bin";
+        };
+      }
+    );
+}
